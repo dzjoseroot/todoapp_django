25b8596c466d6ef7b4b24d0702b3c5d088137054
Jose Diaz <dzjose.root@gmail.com>
2020-07-16 19:47:00 -0500

    initial commit todoapp

---
+++ todoapp/tasks/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('edit/<str:pk>', views.editTask, name="edit"),
+    path('delete/<str:pk>', views.deleteTask, name="delete")
+]
